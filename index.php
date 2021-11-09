2313d0cfe3fda0006342d484ea3cc41e89753878
nguyenloc710 <locnhph16486@fpt.edu.vn>
2021-11-10 00:27:40 +0700

    dao dien php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+    include "view/header.php";
+    if((isset($_GET['act']))&&($_GET['act']!="")){
+        $act=$_GET['act'];
+        switch($act){
+            default:
+                include "view/home.php";
+                break;
+        }
+    }else{
+        include "view/home.php";
+    }
+    include "view/footer.php";
+?>
